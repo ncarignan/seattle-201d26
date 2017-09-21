62957fd1f4b13f6223a2c74b717273bc0fa0ea7e
Sam Hamm <samhamm@gmail.com>
2017-09-21 14:12:44 -0700

    yesterday buildout complete

--- class-04-links-functions-css_layout/buildout-of-yesterday/app.js
+++ class-04-links-functions-css_layout/buildout-of-yesterday/app.js
@@ -2,27 +2,27 @@
 
 // As a developer, I want to add a sixth question to my guessing game that takes numeric input by prompting a user to guess a number (for instance, "What is my favorite number?" or "How many Pokémon did I catch yesterday?"), and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly four opportunities to get the correct answer, so that my fancy programming skills are showcased.
 
-// var guessesLeft = 4;
-// var secretNumber = 8;
-//
-// while (guessesLeft > 0) {
-//   var guess = parseInt(prompt('Guess a number between 1 and 10!'));
-//   if (guess === NaN) {
-//     tell the user
-//   } else if (too high) {
-//     tell the user
-//     reduce number of guesses remaining
-//   } else if (too low) {
-//     tell the user
-//     reduce the guesses remaining
-//   } else {
-//     the must be correct
-//     congratulate them
-//   }
-//   if (user runs out of tries) {
-//     tell them
-//   }
-// }
+var guessesLeft = 4;
+var secretNumber = 8;
+
+while (guessesLeft > 0) {
+  var guess = parseInt(prompt('Guess a number between 1 and 10!'));
+  if (guess.toString() === 'NaN') {
+    alert('Please enter a NUMBER. You entered something else.')
+  } else if (guess > secretNumber) {
+    guessesLeft--;
+    alert('You guessed too high. You have ' + guessesLeft + ' guesses remaining.');
+  } else if (guess < secretNumber) {
+    guessesLeft--;
+    alert('You guessed too low. You have ' + guessesLeft + ' guesses remaining.')
+  } else {
+    guessesLeft = -1;
+    alert('You got it right!!!');
+  }
+  if (guessesLeft === 0) {
+    alert('You used up all of your guesses without guessing the correct number. YOU LOSE.')
+  }
+}
 
 // As a developer, I want to add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
 
@@ -47,7 +47,7 @@ while(guessesRemaining > 0) {
   }
 
   if(guessesRemaining > 0){
-    alert('Nope, that is not a correct answer. You have ' + guessesRemaining +  ' guesses remaining.');
+    alert('Nope, that is not a correct answer. You have ' + guessesRemaining + ' guesses remaining.');
   }
 
   if (guessesRemaining === 0) {
