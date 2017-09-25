6f703e5ff7f7270bbb5b22063ea2930b1e49ba32
Sam Hamm <samhamm@gmail.com>
2017-09-25 10:19:06 -0700

    9/25 middle of class; add DOM demo;

---
+++ class-06-dom-objects-domain_modeling/demo/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thor\'s Day', 'Friday', 'Saturday', 'Sunday'];
+
+// Favorite Seattle beaches
+
+ var alki = {};
+
+var goldenGarden = {};
+
+var edmondsBeach = {};
